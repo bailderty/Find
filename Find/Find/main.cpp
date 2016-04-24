abeefeec64cc94ee4d2f188805283da64eda78e8
bailderty <brettmeyer@Bretts-Mac-mini-6.local>
2016-04-24 09:23:40 -0700

    Created Project

---
+++ Find/Find/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Find
+//
+//  Created by Brett Meyer on 4/24/16.
+//  Copyright © 2016 Brett Meyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
