b87e8a5c0cc25a6f1f1dd6b730f0d284a7d6564a
bailderty <brettmeyer@Bretts-Mac-mini-6.local>
2016-04-24 11:13:03 -0700

    Runs on CSL machines

--- Find/Find/findWord.cpp
+++ Find/Find/findWord.cpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  findWord.cpp
 //  Find
 //
 //  Created by Brett Meyer on 4/24/16.
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
 using namespace std;
 
 int main (int argc, char* argv[]) {
